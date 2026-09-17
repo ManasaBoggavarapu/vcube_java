5a0ab6d2ceebb76b46f603b4e6f5283c9242e696
manasa <manasaboggavarapu52@gmail.com>
2026-09-17 17:13:49 +0530

    methods 1

--- myfirstcorejavaproject/src/com/javaintro/methods/Methods.java
+++ myfirstcorejavaproject/src/com/javaintro/methods/Methods.java
@@ -1,29 +1,29 @@
 package com.javaintro.methods;
 
 public class Methods {
-      void  addition(int a,int b)
-      {
-          int sum=a+b;
-           System.out.println("addition value "+sum);
-           subtraction(sum,30);
-      }
-      void subtraction(int a,int b)
-      {
-    	   int subtraction=a-b;
-    	   System.out.println("Subtraction value  "+subtraction);
-    	     multiplication(subtraction,35 );
-      }
-      void multiplication(int a,int b)
-      {
-    	     int multiplication=a*b;
-    	     System.out.println("Multiplication value "+multiplication);
-    	     division(multiplication,12);
-      }
-      void division(int a,int b)
-      { 
-    	     int division=a/b;
-    	     System.out.println("Division value "+ division);
-      }
+    void  addition(int a,int b)
+    {
+        int sum=a+b;
+         System.out.println("addition value "+sum);
+         subtraction(sum,30);
+    }
+    void subtraction(int a,int b)
+    {
+  	   int subtraction=a-b;
+  	   System.out.println("Subtraction value  "+subtraction);
+  	     multiplication(subtraction,35 );
+    }
+    void multiplication(int a,int b)
+    {
+  	     int multiplication=a*b;
+  	     System.out.println("Multiplication value "+multiplication);
+  	     division(multiplication,12);
+    }
+    void division(int a,int b)
+    { 
+  	     int division=a/b;
+  	     System.out.println("Division value "+ division);
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		    Methods m1=new Methods();
@@ -32,3 +32,4 @@ public class Methods {
 	}
 
 }
+
