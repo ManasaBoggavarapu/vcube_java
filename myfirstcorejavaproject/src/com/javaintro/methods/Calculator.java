fbc560ccf99ad2d399cffafb9e62c8132115bdef
manasa <manasaboggavarapu52@gmail.com>
2026-09-17 16:49:15 +0530

    methods 1 with programs

---
+++ myfirstcorejavaproject/src/com/javaintro/methods/Calculator.java
@@ -0,0 +1,16 @@
+package com.javaintro.methods;
+
+public class Calculator {
+   
+	static void add(int a,int b)
+	{
+		int sum=a+b;
+		System.out.println("add value is"+sum);
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        add(10,20);
+	}
+
+}
---
+++ myfirstcorejavaproject/src/com/javaintro/methods/Methods.java
@@ -0,0 +1,34 @@
+package com.javaintro.methods;
+
+public class Methods {
+      void  addition(int a,int b)
+      {
+          int sum=a+b;
+           System.out.println("addition value "+sum);
+           subtraction(sum,30);
+      }
+      void subtraction(int a,int b)
+      {
+    	   int subtraction=a-b;
+    	   System.out.println("Subtraction value  "+subtraction);
+    	     multiplication(subtraction,35 );
+      }
+      void multiplication(int a,int b)
+      {
+    	     int multiplication=a*b;
+    	     System.out.println("Multiplication value "+multiplication);
+    	     division(multiplication,12);
+      }
+      void division(int a,int b)
+      { 
+    	     int division=a/b;
+    	     System.out.println("Division value "+ division);
+      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    Methods m1=new Methods();
+		    m1.addition(10,20);
+		
+	}
+
+}
